780c20614e82e25578cdcdcce52a06cd5c38a8c1
jrcairns <cairns.webdev@gmail.com>
2023-01-22 16:29:24 -0500

    use env vars for socials

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -34,7 +34,7 @@ export const dropUpVariants = {
 export default function Socials() {
   return (
     <motion.div
-      className='hidden fixed z-10 right-8 inset-y-0 md:flex flex-col justify-center gap-5 items-center'
+      className='fixed inset-y-0 z-10 flex-col items-center justify-center hidden gap-5 right-8 md:flex'
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
@@ -42,27 +42,27 @@ export default function Socials() {
       variants={containerVariants}
     >
       <motion.div variants={dropUpVariants}>
-        <a aria-label='go to discord' target="__blank" href="https://discord.gg/rsjungle" className="rounded-full p-2 bg-gray-50/50 duration-300 hover:bg-gray-50 hover:text-gray-50/70 flex items-center justify-center h-10 w-10 active:scale-95">
+        <a aria-label='go to discord' target="__blank" href={process.env.NEXT_PUBLIC_DISCORD_URL} className="flex items-center justify-center w-10 h-10 p-2 duration-300 rounded-full bg-gray-50/50 hover:bg-gray-50 hover:text-gray-50/70 active:scale-95">
           <img src="/discord.svg" alt="discord logo" />
         </a>
       </motion.div>
       <motion.div variants={dropUpVariants}>
-        <a aria-label='go to twitch' target="__blank" href="https://twitch.tv/rsjungle" className="rounded-full p-2 bg-gray-50/50 duration-300 hover:bg-gray-50 hover:text-gray-50/70 flex items-center justify-center h-10 w-10 active:scale-95">
+        <a aria-label='go to twitch' target="__blank" href={process.env.NEXT_PUBLIC_TWITCH_URL} className="flex items-center justify-center w-10 h-10 p-2 duration-300 rounded-full bg-gray-50/50 hover:bg-gray-50 hover:text-gray-50/70 active:scale-95">
           <img src="/twitch.svg" alt="twitch logo" />
         </a>
       </motion.div>
       <motion.div variants={dropUpVariants}>
-        <a aria-label='go to reddit' target="__blank" href="https://reddit.com/user/RSJungle" className="rounded-full p-2 bg-gray-50/50 duration-300 hover:bg-gray-50 hover:text-gray-50/70 flex items-center justify-center h-10 w-10 active:scale-95">
+        <a aria-label='go to reddit' target="__blank" href={process.env.NEXT_PUBLIC_REDDIT_URL} className="flex items-center justify-center w-10 h-10 p-2 duration-300 rounded-full bg-gray-50/50 hover:bg-gray-50 hover:text-gray-50/70 active:scale-95">
           <img src="/reddit.svg" alt="reddit logo" />
         </a>
       </motion.div>
       <motion.div variants={dropUpVariants}>
-        <a aria-label='go to twitter' target="__blank" href="https://twitter.com/RSJunglecom" className="rounded-full p-2 bg-gray-50/50 duration-300 hover:bg-gray-50 hover:text-gray-50/70 flex items-center justify-center h-10 w-10 active:scale-95">
+        <a aria-label='go to twitter' target="__blank" href={process.env.NEXT_PUBLIC_TWITTER_URL} className="flex items-center justify-center w-10 h-10 p-2 duration-300 rounded-full bg-gray-50/50 hover:bg-gray-50 hover:text-gray-50/70 active:scale-95">
           <img src="/twitter.svg" alt="twitter logo" />
         </a>
       </motion.div>
       <motion.div variants={dropUpVariants}>
-        <a aria-label='go to instagram' target="__blank" href="https://instagram.com/rsjungle" className="rounded-full p-2 bg-gray-50/50 duration-300 hover:bg-gray-50 hover:text-gray-50/70 flex items-center justify-center h-10 w-10 active:scale-95">
+        <a aria-label='go to instagram' target="__blank" href={process.env.NEXT_PUBLIC_INSTAGRAM_URL} className="flex items-center justify-center w-10 h-10 p-2 duration-300 rounded-full bg-gray-50/50 hover:bg-gray-50 hover:text-gray-50/70 active:scale-95">
           <img src="/instagram.svg" alt="instagram logo" />
         </a>
       </motion.div>

6dff21cf77c0fbfa14f1a06bd55ae3f4c3857bd2
jrcairns <cairns.webdev@gmail.com>
2023-01-22 17:48:33 -0500

    add vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AnalyticsWrapper } from '@/components/analytics'
 import './globals.css'
 
 export default function RootLayout({
@@ -7,12 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body className='h-full'>{children}</body>
+      <AnalyticsWrapper />
     </html>
   )
 }

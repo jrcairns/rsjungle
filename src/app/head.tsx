f564e71beb18646d9c5a80f68fecab7ddaf66222
jrcairns <cairns.webdev@gmail.com>
2023-01-22 17:27:03 -0500

    meta image updates

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,19 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="RSJungle.com, the leading RuneScape casino." />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:url" content="https://rsjungle.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Coming Soon - RSJungle" />
+      <meta property="og:description" content="RSJungle.com, the leading RuneScape casino." />
+      <meta property="og:image" content="https://rsjungle.vercel.app/rsjungle-meta.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="rsjungle.vercel.app" />
+      <meta property="twitter:url" content="https://rsjungle.vercel.app" />
+      <meta name="twitter:title" content="Coming Soon - RSJungle" />
+      <meta name="twitter:description" content="RSJungle.com, the leading RuneScape casino." />
+      <meta name="twitter:image" content="https://rsjungle.vercel.app/rsjungle-meta.png" />
     </>
   )
 }
